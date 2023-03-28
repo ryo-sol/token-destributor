7db2f92a763f8614b4123a397817a5a518625d0f
Ryo <ryo.nishimura97@gmail.com>
2023-03-28 16:34:29 +0200

    add accounts

--- programs/token-distributor/src/lib.rs
+++ programs/token-distributor/src/lib.rs
@@ -13,6 +13,7 @@ pub mod token_distributor {
     }
 
     pub fn claim(ctx: Context<ClaimAccounts>) -> Result<()> {
+        //ctx.accounts.user_token_account.to_account_info().try_borrow_data().unwrap().
         Ok(())
     }
 }
@@ -23,7 +24,7 @@ pub struct CreateAccounts<'info> {
     #[account(mut, token::mint = token_mint, token::authority = payer)]
     pub payer_token_account: Account<'info, TokenAccount>,
 
-    #[account(mut, seeds = [b"vault", payer.key().as_ref(), token_mint.key().as_ref()], bump)] 
+    #[account(init, space = VAULT_ACCOUNT_SIZE, payer = payer,  seeds = [b"vault", payer.key().as_ref(), token_mint.key().as_ref()], bump)] 
     pub vault: Account<'info, VaultAccount>,
     
     #[account(init_if_needed, payer = payer, token::mint = token_mint, token::authority = vault)]
@@ -38,15 +39,42 @@ pub struct CreateAccounts<'info> {
 
 #[derive(Accounts)]
 pub struct ClaimAccounts<'info> {
+    pub token_mint: Account<'info, Mint>,
+    #[account(mut, token::mint = token_mint, token::authority = user)]
+    pub user_token_account: Account<'info, TokenAccount>,
+    
+    #[account(mut)]
+    pub user: Signer<'info>,
+    
+    #[account(init, space = CLAIMED_ACCOUNT_SIZE, payer = user, seeds = [b"claimed", user.key().as_ref(), vault.key().as_ref()], bump)] // to prevent double claim we create this account
+    pub user_claimed: Account<'info, ClaimedAccount>,
+    
+    #[account(mut)] 
+    pub vault: Account<'info, VaultAccount>,
+    
+    #[account(mut, token::mint = token_mint, token::authority = vault)]
+    pub vault_token_account: Account<'info, TokenAccount>,
 
     pub token_program:  Program<'info, Token>,
     pub system_program: Program<'info, System>,
 }
 
+static VAULT_ACCOUNT_SIZE : usize = 8+8+32+32;
 
 #[account]
 #[derive(Default)]
 pub struct VaultAccount{
-    data: u64,
-    mint: Pubkey
+    claim_size: u64,
+    mint: Pubkey,
+    authority: Pubkey
+}
+
+static CLAIMED_ACCOUNT_SIZE : usize = 8+1+32+32;
+
+#[account]
+#[derive(Default)]
+pub struct ClaimedAccount{
+    claimed: bool,
+    user: Pubkey,
+    vault: Pubkey,
 }
